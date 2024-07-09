53627ef300e9843cfbd6b209d7adafdf4f33c773
requin-0221 <landshark007@naver.com>
2024-07-09 23:39:00 +0900

    나이트 구현
    나이트 기물 이동 테스트 완료

--- Assets/Script/ChessPieces/Knight.cs
+++ Assets/Script/ChessPieces/Knight.cs
@@ -8,9 +8,49 @@ public class Knight : ChessPiece
     {
         List<Vector2Int> movableCoordinates = new List<Vector2Int>();
 
-        /*
-            여기에 구현
-        */
+        ChessPiece blockingPiece;
+        Vector2Int targetCoordinate;
+        Vector2Int baseCoordinate = coordinate; //기존 위치 저장
+
+        //나이트의 현재 위치로부터 이동 가능한 좌표
+        Vector2Int[] targetPositions = {
+            Vector2Int.up*2 + Vector2Int.left,
+            Vector2Int.up*2 + Vector2Int.right,
+            Vector2Int.right*2 + Vector2Int.up,
+            Vector2Int.right*2 + Vector2Int.down,
+            Vector2Int.down*2 + Vector2Int.right,
+            Vector2Int.down*2 + Vector2Int.left,
+            Vector2Int.left*2 + Vector2Int.down,
+            Vector2Int.left*2 + Vector2Int.up
+            };
+
+        //각 좌표가 이동할 수 있는 칸인지 검사
+        foreach (Vector2Int position in targetPositions)
+        {
+            //현재 좌표에서 이동 가능한 좌표로 targetCoordinate 값 설정
+            targetCoordinate = baseCoordinate + position;
+
+            // targetCoordinate가 체스판 안인지 체크
+            if (_chessData.IsValidCoordinate(targetCoordinate))
+            {
+                //해당 칸에 위치한 기물의 정보를 받아옴
+                blockingPiece = _chessData.GetPiece(targetCoordinate);
+
+                //해당 칸에 위치한 기물이 없을 경우 이동 가능
+                if (blockingPiece == null)
+                {
+                    movableCoordinates.Add(targetCoordinate);
+                }
+                else
+                {
+                    //해당 칸에 적 기물이 있을 경우 이동 가능
+                    if (blockingPiece.pieceColor != this.pieceColor)
+                    {
+                        movableCoordinates.Add(targetCoordinate);
+                    }
+                }
+            }
+        }
 
         return movableCoordinates;
     }
