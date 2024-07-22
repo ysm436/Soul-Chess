9ad31a88fe02f8acf703abb07c9fb19cc144b592
ysm436 <ysm436@gmail.com>
2024-07-23 06:34:53 +0900

    폴더 정리

--- Assets/Script/UI/LobbySceneUI.cs
+++ Assets/Script/UI/LobbySceneUI.cs
@@ -9,18 +9,6 @@ public class LobbySceneUI : MonoBehaviour
 
     [SerializeField] private GameObject PanelName;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     public void ExitButton()
     {
         SceneManager.LoadScene("MatchingScene");
