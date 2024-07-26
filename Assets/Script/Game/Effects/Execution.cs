7be893ceab372e7b5f8896c29878cc7a161d556c
VlOIET <hyunz0904@naver.com>
2024-07-26 22:08:41 +0900

    처형 카드 구현 / targetable 관련 오류 존재

---
+++ Assets/Script/Game/Effects/Execution.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Execution : Effect
+{
+    public int basicAD;
+    public int insteadAD;
+    public int standardHP;
+
+    public override void EffectAction()
+    {
+        foreach (var target in targets)
+        {
+            if ((target as ChessPiece).HP < standardHP)
+            {
+                (target as ChessPiece).HP -= insteadAD;
+            }
+            else
+            {
+                (target as ChessPiece).HP -= basicAD;
+            }
+
+            if(!(target as ChessPiece).isAlive)
+            {
+                GameManager.instance.GetBoardSquare((target as ChessPiece).coordinate).isTargetable = false;
+                GameManager.instance.KillPiece(target as ChessPiece);
+            }
+        }
+    }
+
+}
