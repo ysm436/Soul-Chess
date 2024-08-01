b3daf4ddc0b3fbbed283f89cf9dbdca1710b5bf8
KangHee <rkdgml0820@naver.com>
2024-08-01 11:02:04 +0900

    토르, 프리그, 수르트, 펜니르, 피의 독수리 카드 구현

---
+++ Assets/Script/Game/Card/Cards/Frigg.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class Frigg : SoulCard
+{
+    public int decreaseAmount = 20;
+
+    protected override void Awake()
+    {
+        base.Awake();
+        OnInfuse += (ChessPiece chessPiece) => DecreaseEnemyPiecesAD();
+        OnInfuse += (ChessPiece chessPiece) => GameManager.instance.whiteController.OnOpponentTurnEnd += DecreaseEnemyPiecesAD;
+        OnInfuse += (ChessPiece chessPiece) => GameManager.instance.whiteController.OnMyTurnEnd += IncreaseEnemyPiecesAD;
+    }
+
+    private void IncreaseEnemyPiecesAD()
+    {
+        List<ChessPiece> enemyPieceList = GameManager.instance.gameData.pieceObjects.Where(piece => piece.pieceColor != GameManager.instance.whiteController.playerColor).ToList();
+
+        foreach (ChessPiece piece in enemyPieceList)
+        {
+            piece.AD += decreaseAmount;
+        }
+    }
+
+    private void DecreaseEnemyPiecesAD()
+    {
+        List<ChessPiece> enemyPieceList = GameManager.instance.gameData.pieceObjects.Where(piece => piece.pieceColor != GameManager.instance.whiteController.playerColor).ToList();
+
+        foreach (ChessPiece piece in enemyPieceList)
+        {
+            piece.AD -= decreaseAmount;
+        }
+    }
+}
