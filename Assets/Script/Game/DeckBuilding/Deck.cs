7b8bced83567ce8f8657e670541805c480a6b54e
VlOIET <hyunz0904@naver.com>
2024-08-23 15:33:52 +0900

    카드 필터 세분화 / 그래프 조정
    7코스트 이상의 카드가 많이 존재하여 7/8/9+로 세분화 했습니다.
    세분화에 따라서 덱 정보의 코스트를 나타내는 그래프도 조정했습니다.

--- Assets/Script/Game/DeckBuilding/Deck.cs
+++ Assets/Script/Game/DeckBuilding/Deck.cs
@@ -8,7 +8,7 @@ using UnityEngine;
     public int index;
     public string deckname;
     public int card_count = 0;
-    public int[] costs = new int[8]{0, 0, 0, 0, 0, 0, 0, 0}; // {0, 1, 2, 3, 4, 5, 6, 7+}
+    public int[] costs = new int[10]{0, 0, 0, 0, 0, 0, 0, 0, 0, 0}; // {0, 1, 2, 3, 4, 5, 6, 7+}
     public int[] chesspieces = new int[6]{0, 0, 0, 0, 0, 0}; // {Pawn, Knight, Bishop, Rook, Quene, King}
     public int[] extra_chesspieces = new int[6]{0, 0, 0, 0, 0, 0}; // 다중 기물 카드의 경우 이용되는 chesspiece
     public int[] Rarities = new int[3]{0, 0, 0}; // {Common, Legendary, Rarity}
