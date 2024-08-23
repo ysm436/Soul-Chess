acb1298ce5aa7edac06802392c8fbf543cd6971f
ysm436 <ysm436@gmail.com>
2024-08-24 07:03:44 +0900

    오딘 선택 수정

--- Assets/Script/Game/Effects/SoulEffect/Norse/OdinEffect.cs
+++ Assets/Script/Game/Effects/SoulEffect/Norse/OdinEffect.cs
@@ -1,17 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
+using Photon.Pun;
 using UnityEngine;
 
 public class OdinEffect : Effect
 {
+    private PhotonView photonView;
     [SerializeField] private List<GameObject> selectionCardPrefabList;        //궁니르, 드라우프노르, 감반테인
     private PlayerController Player;
+
+    private void Awake()
+    {
+        photonView = GetComponent<PhotonView>();
+    }
 
     private List<GameObject> selectionCardInstanceList;
     public override void EffectAction(PlayerController player)
     {
         Player = player;
-        InstantiateSelectionCard(gameObject.GetComponent<SoulCard>().InfusedPiece);
+        if (player.playerColor == GameBoard.instance.playerColor)
+            InstantiateSelectionCard(gameObject.GetComponent<SoulCard>().InfusedPiece);
     }
 
     private void InstantiateSelectionCard(ChessPiece chessPiece)
@@ -22,7 +30,7 @@ public class OdinEffect : Effect
             GameObject selectionCardInstance = Instantiate(selectionCard);
             CardSelectionDisplay cardSelectionDisplay = selectionCardInstance.AddComponent<CardSelectionDisplay>();
             cardSelectionDisplay.selectionNumber = selectionCardPrefabList.IndexOf(selectionCard);
-            cardSelectionDisplay.OnSelected += AddCardToHand;
+            cardSelectionDisplay.OnSelected += (selectionNumber) => photonView.RPC("AddCardToHand", RpcTarget.All, selectionNumber);
 
             cardSelectionDisplay.Initialize();
 
@@ -35,9 +43,11 @@ public class OdinEffect : Effect
         }
     }
 
+    [PunRPC]
     private void AddCardToHand(int selectionNumber)
     {
-        Destroy(selectionCardInstanceList[selectionNumber]);
+        if (Player.playerColor == GameBoard.instance.playerColor)
+            Destroy(selectionCardInstanceList[selectionNumber]);
         GameObject selectedcard = Instantiate(selectionCardPrefabList[selectionNumber]);
         Card selectedcard_cardcomponent = selectedcard.GetComponent<Card>();
         selectedcard_cardcomponent.isInSelection = false;
