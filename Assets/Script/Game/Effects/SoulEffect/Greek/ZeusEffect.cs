f2e8d8d3b58b24a53bee242624b15265f079fcf9
VlOIET <hyunz0904@naver.com>
2024-09-01 20:17:34 +0900

    카드가 돌아간 상태로 적의 손에 들어가던 오류 수정

--- Assets/Script/Game/Effects/SoulEffect/Greek/ZeusEffect.cs
+++ Assets/Script/Game/Effects/SoulEffect/Greek/ZeusEffect.cs
@@ -20,6 +20,10 @@ public class ZeusEffect : Effect
             GameObject thunder = Instantiate(thunder_card);
             Card thunder_info = thunder.GetComponent<Card>();
             thunder_info.owner = GetComponent<Card>().owner;
+
+            if (playercolor.playerColor != GameBoard.instance.myController.playerColor)
+                thunder.transform.localEulerAngles = new Vector3(0, 0, 180); //적이 카드를 사용한 경우 카드가 회전해서 상대의 손에 들어가도록 변경
+
             if (!playercolor.TryAddCardInHand(thunder_info))
             {
                 Destroy(thunder);
