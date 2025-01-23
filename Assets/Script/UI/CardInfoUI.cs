438be09ab08d888e8247818e98638d01fc01aa12
milk--ways <joming1418@gmail.com>
2025-01-23 19:28:23 +0900

    덱 선택 창에서 카드목록에 마우스 호버시 카드 정보 나타나는 거 구현

--- Assets/Script/UI/CardInfoUI.cs
+++ Assets/Script/UI/CardInfoUI.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
 public class CardInfoUI : MonoBehaviour
 {
@@ -10,6 +11,7 @@ public class CardInfoUI : MonoBehaviour
     [SerializeField] private GameObject[] costImages;
     [SerializeField] private TextMeshProUGUI cardCostText;
 
+    private Card cardInfo = null;
     public void SetCardInfoUI(Card card)
     {
         cardNameText.text = card.cardName;
@@ -27,5 +29,39 @@ public class CardInfoUI : MonoBehaviour
             costImages[1].SetActive(true);
             cardCostText.color = Color.white;
         }
+
+        cardInfo = card;
+
+        AddEvent();
+    }
+
+    public void AddEvent()
+    {
+        EventTrigger eventTrigger = GetComponent<EventTrigger>();
+
+        EventTrigger.Entry entry_PointerEnter = new EventTrigger.Entry();
+        entry_PointerEnter.eventID = EventTriggerType.PointerEnter;
+        entry_PointerEnter.callback.AddListener((data) => { ShowCardUI(); });
+        eventTrigger.triggers.Add(entry_PointerEnter);
+
+        EventTrigger.Entry entry_PointerExit = new EventTrigger.Entry();
+        entry_PointerExit.eventID = EventTriggerType.PointerExit;
+        entry_PointerExit.callback.AddListener((data) => { HideCardUI(); });
+        eventTrigger.triggers.Add(entry_PointerExit);
+    }
+
+    [SerializeField] private CardUI cardUI;
+
+    public void ShowCardUI()
+    {
+        if (cardInfo == null)
+            return;
+        cardUI.SetCardUI(cardInfo);
+        cardUI.gameObject.SetActive(true);
+    }
+
+    public void HideCardUI()
+    {
+        cardUI.gameObject.SetActive(false);
     }
 }
---
+++ Assets/Script/UI/CardUI.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class CardUI : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI cardName;
+    [SerializeField] private TextMeshProUGUI cost;
+    [SerializeField] private TextMeshProUGUI type;
+    [SerializeField] private TextMeshProUGUI ADText;
+    [SerializeField] private TextMeshProUGUI HPText;
+    [SerializeField] private TextMeshProUGUI description;
+    [SerializeField] private GameObject AD;
+    [SerializeField] private GameObject HP;
+    [SerializeField] private Image illustration;
+    [SerializeField] private GameObject[] cardFrames;
+
+    public void SetCardUI(Card card)
+    {
+        if (card is SoulCard)
+        {
+            type.text = "¿µÈ¥";
+            AD.SetActive(true);
+            HP.SetActive(true);
+            ADText.text = card.GetComponent<SoulCard>().AD.ToString();
+            HPText.text = card.GetComponent<SoulCard>().HP.ToString();
+        }
+        else
+        {
+            type.text = "¸¶¹ý";
+            AD.SetActive(false);
+            HP.SetActive(false);
+        }
+
+        foreach (var cardFrame in cardFrames)
+            cardFrame.SetActive(false);
+
+        cardFrames[(int)card.reigon].SetActive(true);
+
+        cardName.text = card.cardName;
+        cost.text = card.cost.ToString();
+        description.text = card.description;
+
+        Debug.Log("Sprites/Legacy/GameObjects/Card Illustration/" + card.reigon + "/" + card.illustration.name);
+        Debug.Log(Resources.Load("Sprites/Legacy/GameObjects/Card Illustration/" + card.reigon + "/" + card.illustration.name).GetType());
+        
+        illustration.sprite = Resources.Load<Sprite>("Sprites/Legacy/GameObjects/Card Illustration/" + card.reigon + "/" + card.illustration.name);
+    }
+}
