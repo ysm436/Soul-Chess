aa08c5f42566610ba08ed4dfc1d18bd72b899305
VlOIET <hyunz0904@naver.com>
2025-01-25 02:02:19 +0900

    리소스 적용에 필요한 구현
    덱 내 카드를 나타내는 리소스 크기 조절
    턴 종료 버튼의 배경이 자동으로 바뀌게 변경
    '턴 종료' 글씨를 하얀 배경에는 검은색 / 검은 배경에는 하얀색으로
    턴 종료 하이라이트를 쉐이더로 표현 << 눈에 잘 안 띈다고 생각되면 추가로 리소스 필요할 듯

--- Assets/Script/UI/TurnChangeButtonHighlight.cs
+++ Assets/Script/UI/TurnChangeButtonHighlight.cs
@@ -5,26 +5,48 @@ using UnityEngine;
 
 public class TurnChangeButtonHighlight : MonoBehaviour
 {
-    [HideInInspector] public SpriteRenderer spriteRenderer;
-    PlayerController player;
+    private PlayerController player;
+    public Material highlightMaterial;
     private bool isVisible = false;
 
     WaitForSeconds Wait = new WaitForSeconds(0.5f);
 
     void Start()
     {
-        spriteRenderer = GetComponent<SpriteRenderer>();
-        spriteRenderer.enabled = isVisible = false;
+        highlightMaterial = GetComponent<Renderer>().material;
+        isVisible = false;
         player = GameBoard.instance.myController;
+
+        highlightMaterial.SetFloat("_InnerOutlineAlpha", 0f);
+
+        if (player.playerColor == GameBoard.PlayerColor.Black)
+        {   
+            highlightMaterial.SetColor("_InnerOutlineColor", new Color(1, 1, 1, 1));
+            highlightMaterial.SetFloat("_InnerOutlineGlow", 2.5f);
+        }
+        else
+        {
+            highlightMaterial.SetColor("_InnerOutlineColor", new Color(1, 1, 0, 1));
+            highlightMaterial.SetFloat("_InnerOutlineGlow", 1f);
+        }
+
         StartCoroutine(EnableHighlight());
     }
 
     public IEnumerator EnableHighlight()
     {
-        while(true)
+        while (true)
         {
-            if (!isVisible && player.TurnEndPossible && GameBoard.instance.isActivePlayer) spriteRenderer.enabled = isVisible = true;
-            else spriteRenderer.enabled = isVisible = false;
+            if (!isVisible && player.TurnEndPossible && GameBoard.instance.isActivePlayer) 
+            {
+                highlightMaterial.SetFloat("_InnerOutlineAlpha", 1f);
+                isVisible = true;
+            }
+            else
+            {
+                highlightMaterial.SetFloat("_InnerOutlineAlpha", 0f);
+                isVisible = false;
+            }
 
             yield return Wait;
         }
