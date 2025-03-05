53fc3ad37ec8d617194246a3f7ccd4e2f50a136f
VlOIET <hyunz0904@naver.com>
2025-03-05 15:20:30 +0900

    체크 필터 조정

--- Assets/Script/Game/ChessBoard.cs
+++ Assets/Script/Game/ChessBoard.cs
@@ -640,15 +640,15 @@ public class ChessBoard : MonoBehaviour
         {
             tempColor = x;
             colorAdjustments.colorFilter.value = tempColor;
-        }, originTargetColor, 0.5f);
+        }, originTargetColor, 0.8f);
 
         Tween redFilter2 = DOTween.To(() => targetColor, x => 
             {
                 tempColor = x;
                 colorAdjustments.colorFilter.value = tempColor;
-            }, originStartColor, 0.5f);
+            }, originStartColor, 0.8f);
 
-        Tween checkOff = DOVirtual.DelayedCall(0.5f, () => {
+        Tween checkOff = DOVirtual.DelayedCall(0f, () => {
                 checkUI.SetActive(false);
             });
         
