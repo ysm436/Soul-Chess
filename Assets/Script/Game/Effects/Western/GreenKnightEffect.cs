fa80f3c4f47b27b55aec7d8906c057c161c3f6ee
KangHee <rkdgml0820@naver.com>
2025-03-09 14:57:31 +0900

    녹색 기사 리메이크 구현

--- Assets/Script/Game/Effects/Western/GreenKnightEffect.cs
+++ Assets/Script/Game/Effects/Western/GreenKnightEffect.cs
@@ -6,28 +6,26 @@ using UnityEngine;
 public class GreenKnightEffect : TargetingEffect
 {
     [SerializeField] private ChessPiece.PieceType additionalBuffPieceType;
+    private ChessPiece.PieceType targetPieceRestriction =
+            ChessPiece.PieceType.Pawn |
+            ChessPiece.PieceType.Knight |
+            ChessPiece.PieceType.Bishop |
+            ChessPiece.PieceType.Rook |
+            ChessPiece.PieceType.Quene;
+
+    SoulCard soul = null;
+
+    void Awake()
+    {
+        if (soul == null) soul = gameObject.GetComponent<SoulCard>();
+        Predicate<ChessPiece> condition = (ChessPiece piece) => (piece.soul != null) && (piece != soul.InfusedPiece);
+        EffectTarget effectTarget = new EffectTarget(TargetType.Piece, targetPieceRestriction, true, false, condition);
+        targetTypes.Add(effectTarget);
+    }
 
     public override void EffectAction(PlayerController player)
     {
         GreenKnight greenKnightComponent = gameObject.GetComponent<GreenKnight>();
-
-        greenKnightComponent.InfusedPiece.SetKeyword(Keyword.Type.Shield);
-        greenKnightComponent.InfusedPiece.buff.AddBuffByKeyword(greenKnightComponent.cardName, Buff.BuffType.Shield);
-
-        foreach (var target in targets)
-        {
-            if ((target as ChessPiece).Attack(greenKnightComponent.InfusedPiece))
-            {
-                (target as ChessPiece).Move(greenKnightComponent.InfusedPiece.coordinate);
-                GameBoard.instance.chessBoard.KillAnimation((target as ChessPiece), greenKnightComponent.InfusedPiece);
-            }
-            else
-            {
-                (target as ChessPiece).GetComponent<Animator>().SetTrigger("moveTrigger");
-                (target as ChessPiece).GetComponent<Animator>().SetBool("isReturning", true);
-                GameBoard.instance.chessBoard.ForthBackPieceAnimation((target as ChessPiece), greenKnightComponent.InfusedPiece);
-            }
-        }
 
         if (greenKnightComponent.InfusedPiece.pieceType == additionalBuffPieceType)
         {
@@ -35,5 +33,38 @@ public class GreenKnightEffect : TargetingEffect
             greenKnightComponent.InfusedPiece.buff.AddBuffByKeyword(greenKnightComponent.cardName, Buff.BuffType.Shield);
         }
 
+        foreach (var target in targets)
+        {
+            ChessPiece greenKnightPiece = greenKnightComponent.InfusedPiece;
+            ChessPiece targetPiece = target as ChessPiece;
+
+            targetPiece.Attack(greenKnightComponent.InfusedPiece);
+
+            targetPiece.GetComponent<Animator>().SetTrigger("moveTrigger");
+            targetPiece.GetComponent<Animator>().SetBool("isReturning", true);
+            GameBoard.instance.chessBoard.ForthBackPieceAnimation(targetPiece, greenKnightPiece);
+
+            greenKnightPiece.Attack(targetPiece);
+
+            greenKnightPiece.GetComponent<Animator>().SetTrigger("moveTrigger");
+            greenKnightPiece.GetComponent<Animator>().SetBool("isReturning", true);
+            GameBoard.instance.chessBoard.ForthBackPieceAnimation(greenKnightPiece, targetPiece);
+        }
+
     }
+    /*foreach (var target in targets)
+    {
+        if ((target as ChessPiece).Attack(greenKnightComponent.InfusedPiece))
+        {
+            (target as ChessPiece).Move(greenKnightComponent.InfusedPiece.coordinate);
+            GameBoard.instance.chessBoard.KillAnimation((target as ChessPiece), greenKnightComponent.InfusedPiece);
+        }
+        else
+        {
+            (target as ChessPiece).GetComponent<Animator>().SetTrigger("moveTrigger");
+            (target as ChessPiece).GetComponent<Animator>().SetBool("isReturning", true);
+            GameBoard.instance.chessBoard.ForthBackPieceAnimation((target as ChessPiece), greenKnightComponent.InfusedPiece);
+        }
+    }
+}*/
 }
